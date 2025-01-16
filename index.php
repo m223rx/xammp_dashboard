574355b2862cc72ddab604a8ecf1277266dd5acc
m223rx <127022843+m223rx@users.noreply.github.com>
2025-01-16 18:02:34 +0100

    update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start(); // Start the session
 // Check if the user is logged in
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     // Redirect to login page if not logged in
-    header('Location: login.php');
+    header('Location: /auth/login.php');
     exit;
 }
 
