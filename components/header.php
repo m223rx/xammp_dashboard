7a318fde7668622a90d59ec4b6fea96992c8d620
m223rx <127022843+m223rx@users.noreply.github.com>
2025-01-16 21:37:37 +0100

    update

--- components/header.php
+++ components/header.php
@@ -1,24 +1,26 @@
+<?php
+$base_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+?>
+
 <header>
     <div class="headerContainer">
-        <img src="http://192.168.1.121/resources/images/logo.png" alt="" onclick="return redirect('#');">
+        <img src="<?php echo $base_url; ?>/resources/images/logo.png" alt="Logo" onclick="return redirect('#');">
         <div class="navItems">
             <nav>
                 <ul>
-                    <li><a href="../index.php">Home</a></li>
-                    <li><a href="./services/services.php">Services</a></li>
+                    <li><a href="<?php echo $base_url; ?>/index.php">Home</a></li>
+                    <li><a href="<?php echo $base_url; ?>/services/services.php">Services</a></li>
                     <li><a href="http://192.168.1.121/phpmyadmin" target="_blank">PhpMyAdmin</a></li>
                     <?php
                     if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
                         echo '
-                                    <li>
-                                        <form action="index.php" method="post">
-                                            <button type="submit">logout</button>
-                                        </form>
-                                    </li>
-                                ';
+                        <li>
+                            <form action="' . htmlspecialchars($base_url) . '/index.php" method="post">
+                                <button type="submit">Logout</button>
+                            </form>
+                        </li>';
                     }
                     ?>
-
                 </ul>
             </nav>
         </div>
