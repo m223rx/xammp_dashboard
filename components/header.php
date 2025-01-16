80608a2cee9584db6a49dd13d0c9eb0d6f32176f
m223rx <127022843+m223rx@users.noreply.github.com>
2025-01-16 21:43:18 +0100

    update

--- components/header.php
+++ components/header.php
@@ -1,11 +1,14 @@
 <?php
-$base_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']);
+// Define dynamic base URL
+$base_url = 'http://' . $_SERVER['HTTP_HOST'] . str_replace('\\', '/', dirname($_SERVER['PHP_SELF']));
+
+// Ensure the base URL does not end with a slash (except the root "/")
+if (substr($base_url, -1) === '/') {
+    $base_url = rtrim($base_url, '/');
+}
 ?>
 
-<?php
-echo $base_url;
-?>
-
+<?php echo $base_url; ?>
 
 <header>
     <div class="headerContainer">
@@ -15,17 +18,15 @@ echo $base_url;
                 <ul>
                     <li><a href="<?php echo $base_url; ?>/index.php">Home</a></li>
                     <li><a href="<?php echo $base_url; ?>/services/services.php">Services</a></li>
+                    <li><a href="<?php echo $base_url; ?>/services/minecraft/index.php">Minecraft</a></li>
                     <li><a href="http://192.168.1.121/phpmyadmin" target="_blank">PhpMyAdmin</a></li>
-                    <?php
-                    if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
-                        echo '
+                    <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true): ?>
                         <li>
-                            <form action="' . htmlspecialchars($base_url) . '/index.php" method="post">
+                            <form action="<?php echo htmlspecialchars($base_url); ?>/index.php" method="post">
                                 <button type="submit">Logout</button>
                             </form>
-                        </li>';
-                    }
-                    ?>
+                        </li>
+                    <?php endif; ?>
                 </ul>
             </nav>
         </div>
