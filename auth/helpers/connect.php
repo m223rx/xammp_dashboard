43323cb31d9c0cdc756af9c8858fd6d87f78ffe7
m223rx <127022843+m223rx@users.noreply.github.com>
2025-01-17 14:36:20 +0100

    update

--- auth/helpers/connect.php
+++ auth/helpers/connect.php
@@ -1,10 +1,8 @@
 <?php
 
-$conn = mysqli_connect("192.168.1.121", "root@admin", "_mzj@whwvaV2xKRC", "admins");
+$conn = mysqli_connect("192.168.1.102", "root@admin", "_mzj@whwvaV2xKRC", "admins");
 if (mysqli_connect_errno()) {
     printf("Error", mysqli_connect_error());
     exit;
 }
 return $conn;
-
-?>
