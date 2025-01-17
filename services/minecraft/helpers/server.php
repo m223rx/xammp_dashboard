f97843015b61bf5293744704a8eb662ab111b24d
m223rx <127022843+m223rx@users.noreply.github.com>
2025-01-17 19:22:24 +0100

    update

--- services/minecraft/helpers/server.php
+++ services/minecraft/helpers/server.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles/styles.css">
+    <link rel="stylesheet" href="../../../styles/styles.css">
     <script src="helpers/controller.js"></script>
     <script src="https://kit.fontawesome.com/e1130e2a24.js" crossorigin="anonymous"></script>
     <title>Server</title>
